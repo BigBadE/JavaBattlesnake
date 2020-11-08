1c5d546f0a2dcd0e9c1b52a21d42a5ea352b08db
BigBadE <bigbade333@gmail.com>
2020-11-07 19:59:32 -0600

    Fixed vertical directions

--- src/main/java/software/bigbade/battlesnake/game/GameMove.java
+++ src/main/java/software/bigbade/battlesnake/game/GameMove.java
@@ -7,9 +7,9 @@ import software.bigbade.battlesnake.util.Position;
 @Getter
 @RequiredArgsConstructor
 public enum GameMove {
-    UP("up", new Position(0, -1)),
+    UP("up", new Position(0, 1)),
     RIGHT("right", new Position(-1, 0)),
-    DOWN("down", new Position(0, 1)),
+    DOWN("down", new Position(0, -1)),
     LEFT("left", new Position(1, 0));
 
     private final String key;
