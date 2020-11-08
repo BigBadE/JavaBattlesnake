391e8a6a2f34c6ae2ee0e2eda1ab1c41bb0539f2
BigBadE <bigbade333@gmail.com>
2020-11-07 20:00:03 -0600

    More food debugging

--- src/main/java/software/bigbade/battlesnake/ai/AIGetFood.java
+++ src/main/java/software/bigbade/battlesnake/ai/AIGetFood.java
@@ -13,7 +13,9 @@ public class AIGetFood implements IAITask {
     public void executeTask(Map<GameMove, Double> moves, BattlesnakeGame game, Snake snake) {
         //Get closest food
         Position headPosition = game.getSnake().getHead();
+        Battlesnake.info("Checking food");
         for(Position food : game.getFood()) {
+            Battlesnake.info("Found food");
             switch (Integer.compare(food.getX(), headPosition.getX())) {
                 case 1:
                     moves.replace(GameMove.LEFT, moves.get(GameMove.LEFT)*
