cb531f67ef8acca1afed90dc75b7d0c6b994c659
BigBadE <bigbade333@gmail.com>
2020-11-08 09:29:17 -0600

    Fixed fillArea

--- src/main/java/software/bigbade/battlesnake/ai/TaskDontTrapYourself.java
+++ src/main/java/software/bigbade/battlesnake/ai/TaskDontTrapYourself.java
@@ -21,12 +21,8 @@ public class TaskDontTrapYourself implements IAITask {
             if (moves.get(move) == 0) {
                 continue;
             }
-            int empty = fillArea(move.getRelative(snake.getHead()), game.getBoard().clone());
+            int empty = fillArea(move.getRelative(snake.getHead()), game.getBoard());
             Battlesnake.info("EMPTY: {} for {}", empty, move);
-            if (empty == 0) {
-                moves.replace(move, .01d);
-                continue;
-            }
             moves.replace(move, game.getSize().getX() * game.getSize().getY() / empty * 5 * moves.get(move));
         }
 
@@ -39,46 +35,51 @@ public class TaskDontTrapYourself implements IAITask {
     public static int fillArea(Position position, boolean[][] arr) {
         int maxX = arr.length - 1;
         int maxY = arr[0].length - 1;
-        int[][] stack = new int[(maxX+1)*(maxY+1)][2];
+        boolean[][] checked = new boolean[maxX + 1][maxY + 1];
+        int[][] stack = new int[(maxX + 1) * (maxY + 1)][2];
         int index = 0;
         int found = 0;
 
         stack[0][0] = position.getX();
         stack[0][1] = position.getY();
-        arr[position.getX()][position.getY()] = true;
+        checked[position.getX()][position.getY()] = true;
 
-        while (index >= 0){
+        while (index >= 0) {
             position.setX(stack[index][0]);
             position.setY(stack[index][1]);
             index--;
             found++;
 
-            if ((position.getX() > 0) && !arr[position.getX() - 1][position.getY()]){
-                arr[position.getX()-1][position.getY()] = true;
+            if ((position.getX() > 0) && !arr[position.getX() - 1][position.getY()]
+                    && !checked[position.getX() - 1][position.getY()]) {
+                checked[position.getX() - 1][position.getY()] = true;
                 index++;
-                stack[index][0] = position.getX()-1;
+                stack[index][0] = position.getX() - 1;
                 stack[index][1] = position.getY();
             }
 
-            if ((position.getX() < maxX) && !arr[position.getX()+1][position.getY()]){
-                arr[position.getX()+1][position.getY()] = true;
+            if ((position.getX() < maxX) && !arr[position.getX() + 1][position.getY()]
+                    && !checked[position.getX() + 1][position.getY()]) {
+                checked[position.getX() + 1][position.getY()] = true;
                 index++;
-                stack[index][0] = position.getX()+1;
+                stack[index][0] = position.getX() + 1;
                 stack[index][1] = position.getY();
             }
 
-            if ((position.getY() > 0) && !arr[position.getX()][position.getY()-1]){
-                arr[position.getX()][position.getY()-1] = true;
+            if ((position.getY() > 0) && !arr[position.getX()][position.getY() - 1]
+                    && !checked[position.getX() + 1][position.getY()]) {
+                checked[position.getX()][position.getY() - 1] = true;
                 index++;
                 stack[index][0] = position.getX();
-                stack[index][1] = position.getY()-1;
+                stack[index][1] = position.getY() - 1;
             }
 
-            if ((position.getY() < maxY) && !arr[position.getX()][position.getY()+1]){
-                arr[position.getX()][position.getY()+1] = true;
+            if ((position.getY() < maxY) && !arr[position.getX()][position.getY() + 1]
+                    && !checked[position.getX()][position.getY() + 1]) {
+                checked[position.getX()][position.getY() + 1] = true;
                 index++;
                 stack[index][0] = position.getX();
-                stack[index][1] = position.getY()+1;
+                stack[index][1] = position.getY() + 1;
             }
         }
         return found;
